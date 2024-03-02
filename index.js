a26583537c9587ea62e78d542808f0c5244760b7
James DivineMercy <iamoluchimercy@gmail.com>
2024-03-02 18:06:35 +0100

    new update

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const off = document.querySelector('.off');
 const buttons = document.querySelector('.buttons');
 const screen = document.querySelector('.screen');
 const allButton = document.querySelector('#allButtons')
+const hide = document.querySelector(".typing-animation");
+const operator = document.querySelector(".operator");
+
 
 open.addEventListener('click', () => {
   backcontainer.style.display = 'block';
+  hide.style.display = 'block';
 
   const frontContainerRect = container.getBoundingClientRect();
   
@@ -24,11 +28,24 @@ open.addEventListener('click', () => {
 off.addEventListener('click', () => {
   buttons.classList.remove('slide-up');
   buttons.classList.add('slide-down');
+
   setTimeout(() => {
     backcontainer.style.display = 'none';
+      hide.style.display = 'none';
     container.style.display = 'block'; 
   }, 300);
 });
 
+allButton.addEventListener('click',(event) =>{
+  event.preventDefault();
+  let value;
+  if(event.target.className == 'numbers'){
+value = event.target.textContent;
+  }
+
+  hide.textContent = value;
+  hide.style.fontSize = "25px";
+
+})
 
 
